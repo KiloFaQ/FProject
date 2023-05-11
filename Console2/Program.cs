a4e135c3ace8e44621537c97c9822e403e3c582c
KiloFaQ <kilo.rino@mail.ru>
2023-05-11 21:16:12 +0700

    all project

---
+++ Console2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Выберите операцию (+, -, *, /, ^): ");
+        char operation = char.Parse(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '^':
+                result = Math.Pow(num1, num2);
+                break;
+            default:
+                Console.WriteLine("Неверная операция");
+                break;
+        }
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
---
+++ Console3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Circle
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double Area()
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+
+    public double Circumference()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+
+        Console.WriteLine("Площадь круга: " + circle.Area());
+        Console.WriteLine("Длина окружности: " + circle.Circumference());
+    }
+}
