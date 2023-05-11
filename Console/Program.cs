cf9a10f601cae2bf53c4d2738ebe29012c95b201
KiloFaQ <kilo.rino@mail.ru>
2023-05-11 21:06:03 +0700

    5.2

---
+++ Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > 1)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(num + " не является простым числом");
+                    break;
+                }
+                else if (i == num - 1)
+                {
+                    Console.WriteLine(num + " является простым числом");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine(num + " не является простым числом");
+        }
+    }
+}
