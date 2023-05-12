1061fa9bed97fb20c38241eaf49e06e0da18633f
KiloFaQ <kilo.rino@mail.ru>
2023-05-12 21:57:48 +0700

    Bubble

---
+++ ConsoleAll/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Сколько чисел будем сортировать?");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите числа для сортировки");
+        int[] mas=new int[N];
+        for (int i = 0; i < mas.Length; i++)
+        {
+            mas[i]= Convert.ToInt32(Console.ReadLine());
+        }
+        BubbleSort(mas);
+        Console.WriteLine("После сортировки");
+        for (int i = 0; i < mas.Length; i++)
+        {
+            Console.WriteLine(mas[i]);
+        }
+        Console.ReadLine();
+            
+    }
+
+    private static void BubbleSort(int[] mas)
+    {
+        throw new NotImplementedException();
+    }
+}
